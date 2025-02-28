de85f450cd953412ea0a3357ed20c6a35fd59470
Nduong <nguyenduong21072009@gmail.com>
2025-02-28 09:38:34 +0700

    Update appstate.js

--- appstate.js
+++ appstate.js
@@ -1,1 +1,83 @@
-//thích thì add appstate vô để cho nó chạy command
+[
+	{
+		"key": "datr",
+		"value": "v420Z6lOFIUfYwKm4bDrlv2K",
+		"domain": "facebook.com",
+		"path": "/",
+		"hostOnly": false,
+		"creation": "2025-02-28T02:37:54.004Z",
+		"lastAccessed": "2025-02-28T02:37:54.011Z"
+	},
+	{
+		"key": "sb",
+		"value": "v420Z5P5Rvnl6E5K-5hIjhlO",
+		"domain": "facebook.com",
+		"path": "/",
+		"hostOnly": false,
+		"creation": "2025-02-28T02:37:54.011Z",
+		"lastAccessed": "2025-02-28T02:37:54.011Z"
+	},
+	{
+		"key": "ps_l",
+		"value": "1",
+		"domain": "facebook.com",
+		"path": "/",
+		"hostOnly": false,
+		"creation": "2025-02-28T02:37:54.011Z",
+		"lastAccessed": "2025-02-28T02:37:54.011Z"
+	},
+	{
+		"key": "ps_n",
+		"value": "1",
+		"domain": "facebook.com",
+		"path": "/",
+		"hostOnly": false,
+		"creation": "2025-02-28T02:37:54.011Z",
+		"lastAccessed": "2025-02-28T02:37:54.011Z"
+	},
+	{
+		"key": "wd",
+		"value": "1322x616",
+		"domain": "facebook.com",
+		"path": "/",
+		"hostOnly": false,
+		"creation": "2025-02-28T02:37:54.011Z",
+		"lastAccessed": "2025-02-28T02:37:54.011Z"
+	},
+	{
+		"key": "c_user",
+		"value": "61573539636959",
+		"domain": "facebook.com",
+		"path": "/",
+		"hostOnly": false,
+		"creation": "2025-02-28T02:37:54.011Z",
+		"lastAccessed": "2025-02-28T02:37:54.011Z"
+	},
+	{
+		"key": "xs",
+		"value": "48%3AiNENDsYmPUsX-Q%3A2%3A1740626801%3A-1%3A-1%3A%3AAcVvo2XuLa9v8qXYBjcdQjLRV8chhi_Auf3LAvcXQA",
+		"domain": "facebook.com",
+		"path": "/",
+		"hostOnly": false,
+		"creation": "2025-02-28T02:37:54.011Z",
+		"lastAccessed": "2025-02-28T02:37:54.011Z"
+	},
+	{
+		"key": "fr",
+		"value": "12XL0CkXE1c1eR7jN.AWWDUn1EMRmvOf7ffd6vyA7rLuvylQJQFisu7A.BnwRl5..AAA.0.0.BnwSF3.AWXuJeYG0IA",
+		"domain": "facebook.com",
+		"path": "/",
+		"hostOnly": false,
+		"creation": "2025-02-28T02:37:54.011Z",
+		"lastAccessed": "2025-02-28T02:37:54.011Z"
+	},
+	{
+		"key": "presence",
+		"value": "C%7B%22t3%22%3A%5B%5D%2C%22utc3%22%3A1740710269413%2C%22v%22%3A1%7D",
+		"domain": "facebook.com",
+		"path": "/",
+		"hostOnly": false,
+		"creation": "2025-02-28T02:37:54.011Z",
+		"lastAccessed": "2025-02-28T02:37:54.011Z"
+	}
+]//thích thì add appstate vô để cho nó chạy command
